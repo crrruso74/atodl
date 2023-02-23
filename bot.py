75da46ba77f74652ce41d1063c99c5d1baa8034c
iam <121612750+Sssskkkyyy3@users.noreply.github.com>
2023-02-23 13:36:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,14 @@ from pyrogram import Client, filters, idle
 
 API_ID = int(environ.get("API_ID", 24281454))
 API_HASH = environ.get("API_HASH", "dbe4521b4291da85becb65c7d4d4c36c")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5870947316:AAFiAwbi-cEGelZ5Mac6ovgg76mBD_XZoMI")
-SESSION = environ.get("SESSION", "BQApuWwJxNsN9lwA_5UYTSyU-ujcoGQY5ethBnTaJoc7EULiur_7j4pICvCL3z5SrmcOkOK3bbwArZGfCEQ3neWEYl82ExwABRW7rGWrv1YWiKMCVhwsGHAKDmQT1uBW31ZTdwo3_TORjY2GAbtyOJT3aTAkhLYdoeFaJkHit3X2L0NrMnq5sy4JoasVN3_1mifZHbp0e0mBfueQ_QateYHJ9nf37sSaScafVpH2oz7GoYYlVSmjpegaX7vSlKBfCvcd0TXL40hfUlzcNXbgOiB6RE4_1zOgx4X0UwtjqVMiYVJ3A2SOoRi7KMtOaspQVci26hXYafvY_kqIAeCt19wKAAAAAV-wO78A")
-TIME = int(environ.get("TIME", 250))
+BOT_TOKEN = environ.get("BOT_TOKEN", "5460795442:AAGn6kth_XPCMO1KIOpB8-odZX_h21Ujl1g")
+SESSION = environ.get("SESSION", "")
+TIME = int(environ.get("TIME", 3))
 GROUPS = []
-for grp in environ.get("GROUPS", "-1001489648140 -1001638282030 -1001645982114").split():
+for grp in environ.get("GROUPS", "-1001652878224 -1001762138428").split():
     GROUPS.append(int(grp))
 ADMINS = [5650200786]
-for usr in environ.get("ADMINS", "5650200786").split():
+for usr in environ.get("ADMINS", "1119774110").split():
     ADMINS.append(int(usr))
 
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
